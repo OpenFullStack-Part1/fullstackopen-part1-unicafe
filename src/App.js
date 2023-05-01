075e475a251ca897445a7fac18adabf745dc94a7
Tanguy Xavier <tanguy.xavier97@gmail.com>
2023-05-01 11:07:13 +0200

    end of unicafe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "./components/Button";
+import StatisticLine from "./components/StatisticLine";
+import "./components/css/App.css";
 
-function App() {
+const Statistics = (props) => {
+  const { good, neutral, bad, allFeedback, score } = props;
+  const average = allFeedback === 0 ? 0 : score / allFeedback;
+  const positiveAverage = allFeedback === 0 ? 0 : (100 * good) / allFeedback;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Statistics</h2>
+      <table>
+      <tbody>
+        <tr>
+          <th>Good</th>
+          <th>Neutral</th>
+          <th>Bad</th>
+          <th>All Feedback</th>
+          <th>Average</th>
+          <th>Positive</th>
+        </tr>
+        <tr>
+          <td>
+            <StatisticLine value={good} />
+          </td>
+          <td>
+            <StatisticLine value={neutral} />
+          </td>
+          <td>
+            <StatisticLine value={bad} />
+          </td>
+          <td>
+            <StatisticLine value={allFeedback} />
+          </td>
+          <td>
+            <StatisticLine value={average.toFixed(1)} />
+          </td>
+          <td>
+            <StatisticLine value={positiveAverage + "%"} />
+          </td>
+        </tr>
+      </tbody>
+      </table>
     </div>
   );
-}
+};
+
+const App = () => {
+  // enregistrer les clics de chaque bouton dans un état différent
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allFeedback, setAllFeedback] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const setToGood = (newGood) => {
+    console.log("good now", newGood);
+    setGood(newGood);
+    setAllFeedback(allFeedback + 1);
+    setScore(score + 1);
+  };
+
+  const setToNeutral = (newNeutral) => {
+    console.log("neutral now", newNeutral);
+    setNeutral(newNeutral);
+    setAllFeedback(allFeedback + 1);
+  };
+
+  const setToBad = (newBad) => {
+    console.log("bad now", newBad);
+    setBad(newBad);
+    setAllFeedback(allFeedback + 1);
+    setScore(score - 1);
+  };
+
+  return (
+    <div>
+      <h1>Give FeedBack</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="Good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="Bad" />
+      {allFeedback !== 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          allFeedback={allFeedback}
+          score={score}
+        />
+      ) : (
+        <p>No Feedback given</p>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/StatisticLine.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StatisticLine = (props) => (
+    <div>
+      {props.text}
+      {props.value}
+    </div>
+  );
+
+export default StatisticLine;
